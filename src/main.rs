a6c7895db4f8f03805240440943cbcfde5049460
nhjschulz <github@schulznorbert.de>
2024-04-12 16:09:52 +0200

    Scheduler: Support mutable task execution

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,20 @@ struct PrintExecuter {
     msg: &'static str,
 }
 impl lwos::Execute for PrintExecuter {
-    fn execute(&self, _id: lwos::TaskId) {
+    fn execute(&mut self, _id: lwos::TaskId) {
         println!("{}", self.msg);
+    }
+}
+
+/// Example for mutable executer
+struct CountExecuter {
+    count: usize,
+}
+
+impl lwos::Execute for CountExecuter {
+    fn execute(&mut self, _id: lwos::TaskId) {
+        println!("CountExecuter {}", self.count);
+        self.count = self.count + 1;
     }
 }
 
@@ -14,10 +26,12 @@ fn main() {
     let mut hello_executer = PrintExecuter { msg: "Hello" };
     let mut scheduler_executer = PrintExecuter { msg: "scheduler" };
     let mut world_executer = PrintExecuter { msg: "world!\r\n" };
+    let mut counter_executer = CountExecuter { count: 0usize };
 
     let mut hello_task = lwos::Task::new(lwos::TaskState::Running, &mut hello_executer);
     let mut scheduler_task = lwos::Task::new(lwos::TaskState::Running, &mut scheduler_executer);
     let mut world_task = lwos::Task::new(lwos::TaskState::Running, &mut world_executer);
+    let mut counter_task = lwos::Task::new(lwos::TaskState::Running, &mut counter_executer);
 
     let mut task_ids: [lwos::TaskId; TASKS] = [lwos::INVALID_ID; TASKS];
     let mut scheduler: lwos::Scheduler<TASKS> = lwos::Scheduler::new();
@@ -25,6 +39,7 @@ fn main() {
     task_ids[0] = scheduler.add(&mut hello_task).unwrap();
     task_ids[1] = scheduler.add(&mut scheduler_task).unwrap();
     task_ids[2] = scheduler.add(&mut world_task).unwrap();
+    task_ids[3] = scheduler.add(&mut counter_task).unwrap();
 
     scheduler.process(); // prints "hello scheduler world!
     scheduler.get(task_ids[1]).unwrap().suspend(); // disable "scheduler" print task
